d133e290b7ead82382fc06e2458a2ac680642a6c
sumitsasidharan <s.sumit196@gmail.com>
2022-10-02 12:29:11 +0530

    add more charts

--- main.js
+++ main.js
@@ -74,6 +74,9 @@ mainSeries.applyOptions({
    borderVisible: false,
 });
 
+
+
+
 /****************************  
       PRICE FORMAT
 *****************************/
@@ -142,7 +145,7 @@ chart.applyOptions({
 });
 
 /****************************  
-   ADDING A SECOND SERIES
+   ADDING A SECOND SERIES (LINE SERIES)
 *****************************/
 // Generate sample data to use within a candlestick series
 // const candleStickData = generateCandlestickData();
@@ -178,6 +181,139 @@ chart.applyOptions({
       fontFamily: "'Roboto', sans-serif",
    },
 });
+
+
+
+
+/****************************  
+   BAR SERIES CHART
+*****************************/
+// Create the Lightweight Chart within the container element
+// const barContainer = document.getElementById('container');
+// const barChart = LightweightCharts.createChart(
+//    barContainer,
+//    chartContainerLayoutOptions
+// );
+
+// const barSeries = barChart.addBarSeries();
+// barSeries.setData(candleStickData);
+
+
+const chartOptions = {
+   layout: {
+      textColor: 'black',
+      background: { type: 'solid', color: 'white' },
+   },
+};
+const barChart = LightweightCharts.createChart(
+   document.getElementById('bar'),
+   chartOptions
+);
+const barSeries = barChart.addBarSeries({
+   upColor: '#26a69a',
+   downColor: '#ef5350',
+});
+
+const data = [
+   { open: 10, high: 10.63, low: 9.49, close: 9.55, time: 1642427876 },
+   { open: 9.55, high: 10.3, low: 9.42, close: 9.94, time: 1642514276 },
+   { open: 9.94, high: 10.17, low: 9.92, close: 9.78, time: 1642600676 },
+   { open: 9.78, high: 10.59, low: 9.18, close: 9.51, time: 1642687076 },
+   { open: 9.51, high: 10.46, low: 9.1, close: 10.17, time: 1642773476 },
+   { open: 10.17, high: 10.96, low: 10.16, close: 10.47, time: 1642859876 },
+   { open: 10.47, high: 11.39, low: 10.4, close: 10.81, time: 1642946276 },
+   { open: 10.81, high: 11.6, low: 10.3, close: 10.75, time: 1643032676 },
+   { open: 10.75, high: 11.6, low: 10.49, close: 10.93, time: 1643119076 },
+   { open: 10.93, high: 11.53, low: 10.76, close: 10.96, time: 1643205476 },
+];
+
+barSeries.setData(data);
+
+barChart.timeScale().fitContent();
+
+
+/****************************  
+   BASELINE SERIES CHART
+*****************************/
+// const chartOptions = {
+//    layout: {
+//       textColor: 'black',
+//       background: { type: 'solid', color: 'white' },
+//    },
+// };
+const baselineChart = LightweightCharts.createChart(
+   document.getElementById('baseline'),
+   chartOptions
+);
+const baselineSeries = baselineChart.addBaselineSeries({
+   baseValue: { type: 'price', price: 25 },
+   topLineColor: 'rgba( 38, 166, 154, 1)',
+   topFillColor1: 'rgba( 38, 166, 154, 0.28)',
+   topFillColor2: 'rgba( 38, 166, 154, 0.05)',
+   bottomLineColor: 'rgba( 239, 83, 80, 1)',
+   bottomFillColor1: 'rgba( 239, 83, 80, 0.05)',
+   bottomFillColor2: 'rgba( 239, 83, 80, 0.28)',
+});
+
+const baselineData = [
+   { value: 1, time: 1642425322 },
+   { value: 8, time: 1642511722 },
+   { value: 10, time: 1642598122 },
+   { value: 20, time: 1642684522 },
+   { value: 3, time: 1642770922 },
+   { value: 43, time: 1642857322 },
+   { value: 41, time: 1642943722 },
+   { value: 43, time: 1643030122 },
+   { value: 56, time: 1643116522 },
+   { value: 46, time: 1643202922 },
+];
+
+baselineSeries.setData(baselineData);
+
+baselineChart.timeScale().fitContent();
+
+
+/****************************  
+   HISTOGRAM SERIES CHART
+*****************************/
+// const chartOptions = {
+//    layout: {
+//       textColor: 'black',
+//       background: { type: 'solid', color: 'white' },
+//    },
+// };
+const histogramChart = LightweightCharts.createChart(
+   document.getElementById('histogram'),
+   chartOptions
+);
+const histogramSeries = histogramChart.addHistogramSeries({ color: '#26a69a' });
+
+const histogramData = [
+   { value: 1, time: 1642425322 },
+   { value: 8, time: 1642511722 },
+   { value: 10, time: 1642598122 },
+   { value: 20, time: 1642684522 },
+   { value: 3, time: 1642770922, color: 'red' },
+   { value: 43, time: 1642857322 },
+   { value: 41, time: 1642943722, color: 'red' },
+   { value: 43, time: 1643030122 },
+   { value: 56, time: 1643116522 },
+   { value: 46, time: 1643202922, color: 'red' },
+];
+
+histogramSeries.setData(histogramData);
+
+chart.timeScale().fitContent();
+
+
+
+
+
+
+
+
+
+
 
 // Adding a window resize event handler to resize the chart when
 // the window size changes.
