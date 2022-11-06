a61fa775bc799a20ca3afc61f156c90f5dc01038
sumitsasidharan <s.sumit196@gmail.com>
2022-11-06 20:25:23 +0530

    deleted some files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import Form from './components/Form';
-import Post from './components/Post';
 import MultiInputList from './components/TniTask/MultiInputList';
 
 function App() {
    return (
       <div className="App">
-         {/* <Post /> */}
-         {/* <Form /> */}
          <MultiInputList />
       </div>
    );
